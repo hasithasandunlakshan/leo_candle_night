11b595d444266dbfc8b308596eff71e62ffb1f3a
hasithasandun <hasiofficial2002@gmail.com>
2024-09-20 13:53:49 +0530

    Initial

---
+++ src/components/Home/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BackgroundBeamsWithCollision } from '../ui/background-beams-with-collision'
+import Navbar from '../Nav/Navbar'
+
+export default function Hero() {
+  return (
+    <BackgroundBeamsWithCollision>
+
+<Navbar/>
+
+        <h1 className=' text-secondary'>Candle Night</h1>
+    </BackgroundBeamsWithCollision>
+  )
+}
