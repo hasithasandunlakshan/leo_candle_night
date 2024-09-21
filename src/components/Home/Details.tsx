e697b253b58c0568f00732934952d212ddf13eac
hasithasandun <hasiofficial2002@gmail.com>
2024-09-21 20:19:03 +0530

    margin 0

--- src/components/Home/Details.tsx
+++ src/components/Home/Details.tsx
@@ -3,11 +3,34 @@ import { PinContainer } from '../ui/3d-pin'
 import { CgCalendarDates } from "react-icons/cg";
 import { FaLocationDot } from "react-icons/fa6";
 import { MdAccessTime } from "react-icons/md";
+import { motion
+
+ } from 'framer-motion';
 export default function Details() {
   return (
-    <div  className=' bg-primary relative items-center justify-center  h-full flex-col min-h-screen ' >
- <div className="absolute top-40 -left-20 w-60 h-60 bg-gradient-to-r from-yellow-400 to-amber-500 rounded-full filter blur-3xl opacity-10"></div>
- <div className="absolute top-20 right-20 w-96 h-96 bg-gradient-to-r from-yellow-400 to-amber-500 rounded-full filter blur-3xl opacity-20"></div>
+    <div  className=' bg-primary relative items-center flex justify-center  h-full flex-col min-h-screen ' >
+
+<motion.div className="flex w-[70%]    gap-3 items-center justify-center"
+
+initial={{ opacity: 0, y: 100 }}
+whileInView={{ opacity: 1, y: 0 }}
+viewport={{ once: true }}
+transition={{ duration: 1}}
+>
+<h1 className="text-center text-3xl  text-white md:text-6xl ">
+  Organized By the  
+  <span className="mx-5 font-Allura text-4xl  md:text-8xl font-bold text-secondary">
+    Leo Club
+  </span>
+  of University Of Moratuwa
+</h1>
+
+</motion.div>
+
+     
+ <div className="absolute w-52 h-40 bottom-8  left-0 sm:w-96 sm:h-96 bg-gradient-to-r from-yellow-400 to-amber-500 rounded-full filter blur-3xl opacity-20"></div>
+       <div className="absolute top-0 right-0  w-52 h-40     sm:w-72 sm:h-72 bg-gradient-to-r from-yellow-400 to-amber-500 rounded-full filter blur-3xl opacity-30"></div>
+
 <div className="flex justify-center items-center  container flex-col align-middle    lg:flex-row ">
 
 
--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -26,18 +26,18 @@ export default function Hero() {
   return (
    
     <div className="min-h-screen  w-full  flex items-center align-middle justify-center bg-primary relative overflow-hidden">
-       <div className="absolute  bottom-10 left-10 w-60 h-60 bg-gradient-to-r from-yellow-400 to-amber-500 rounded-full filter blur-3xl opacity-30"></div>
-       <div className="absolute top-20 right-20 w-60 h-60 bg-gradient-to-r from-yellow-400 to-amber-500 rounded-full filter blur-3xl opacity-30"></div>
+       <div className="absolute w-52 h-40 bottom-8  left-0 sm:w-96 sm:h-96 bg-gradient-to-r from-yellow-400 to-amber-500 rounded-full filter blur-3xl opacity-20"></div>
+       <div className="absolute top-0 right-0  w-52 h-40     sm:w-72 sm:h-72 bg-gradient-to-r from-yellow-400 to-amber-500 rounded-full filter blur-3xl opacity-30"></div>
     {/* <Spotlight
       className="-top-40 left-0 md:left-60 md:-top-20"
       fill="#d6ab31"
     /> */}
 <div className="flex justify-center  container items-center">
-<div className="flex flex-col w-[90%] md:w-1/2 items-center md:items-start py-32">
+<div className="flex flex-col w-[90%] md:w-1/2  lg:w-[40%] items-center md:items-start py-32">
     {/* <TypewriterEffectSmooth words={words} cursorClassName='hidden' /> */}
     <TextGenerateEffect words={description2} className='text-secondary  font-Allura text-5xl sm:text-6xl  md:text-8xl font-bold' duration={2} />
    
-    <TextGenerateEffect words={description} className='text-gray-200 text-center sm:text-left text-3xl sm:text-4xl font-bold mb-2' duration={3} />
+    <TextGenerateEffect words={description} className='text-gray-200 text-center sm:text-left text-3xl sm:text-4xl font-bold mb-2' duration={1} />
    
   <motion.p 
    initial={{ opacity: 0, y: 50 }}
@@ -54,7 +54,7 @@ export default function Hero() {
  initial={{ opacity: 0, }}
  whileInView={{ opacity: 1,  }}
  viewport={{ once: true }}
- transition={{ duration: 10 }}
+ transition={{ duration: 3}}
   className="relative py-1 mt-10 px-10 max-w-50 text-black text-base font-bold nded-full overflow-hidden bg-secondary rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-amber-500 before:to-amber-400 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0"
 >
   Book Ticket
@@ -62,10 +62,15 @@ export default function Hero() {
 
 
     </div>
-    <div className="sm:flex hidden">
-      <div className="sm:flex hidden">
+    <div className="sm:flex hidden   md:ml-20   items-center  justify-center align-middle">
+      <motion.div className="sm:flex hidden"
+       initial={{ opacity: 0,  }}
+       whileInView={{ opacity: 1, }}
+       viewport={{ once: true }}
+       transition={{ duration: 8 }}
+      >
       <Image src={"/images/candle.png"} width={500} height={40} alt='candle' className=' rounded-2xl'/>
-      </div>
+      </motion.div>
  
     </div>
 </div>
