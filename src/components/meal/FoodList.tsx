b0426194e450024821ea0a6934d90c6c94febf48
hasithasandun <hasiofficial2002@gmail.com>
2024-10-07 19:10:06 +0530

    Food select

--- src/components/meal/FoodList.tsx
+++ src/components/meal/FoodList.tsx
@@ -1,6 +1,7 @@
 
 import React, { useState } from 'react';
 import FoodCard from './FoodCard';
+import { Button } from '../ui/button';
 
 const foods = [
   { id: 1, name: 'Pizza', price: 9.99 },
@@ -13,39 +14,41 @@ const foods = [
 
 
 interface FoodListProps {
-    // food: {
-    //   id: number;
-    //   name: string;
-    //   price: number;
-    // };
-    // onAddToCart: (food: { id: number; name: string; price: number }) => void;
+  
 
-    onSelectFood: (food: { id: number; name: string; price: number }) => void 
+    // onSelectFood: (food: { id: number; name: string; price: number }) => void 
 
-
+    FinalFood:(cart:any)=>void
   }
+ 
 
   
-const FoodList:React.FC<FoodListProps> = ({ onSelectFood }) => {
+const FoodList:React.FC<FoodListProps> = ({ FinalFood }) => {
   const [cart, setCart] = useState<any[]>([]);
   const handleAddToCart = (food: { id: number; name: string; price: number }) => {
     setCart((prevCart) => [...prevCart,food]);
     alert(`${food.name} added to cart!`);
   };
 
+  const handleConfirm = () => {
+    FinalFood(cart); // Pass selected food items to the parent component
+  };
   return (
     <div className="flex flex-wrap justify-center ">
       {foods.map((food) => (
         <FoodCard key={food.id} food={food} onAddToCart={handleAddToCart} />
       ))}
-      <h2 className="mt-4 text-xl font-bold">Cart Items:</h2>
+      {/* <h2 className="mt-4 text-xl font-bold">Cart Items:</h2>
       <ul className="mt-2">
         {cart.map((item, index) => (
           <li key={index} className="text-gray-700">
             {item.name} - ${item.price.toFixed(2)}
           </li>
         ))}
-      </ul>
+      </ul> */}
+
+
+      <Button onClick={handleConfirm} className=' bg-white text-black'>Confirm</Button>
     </div>
   );
 };
