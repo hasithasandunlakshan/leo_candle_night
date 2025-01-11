abfeb236665b3501678202448cc984a0a959728a
Chamindu24 <chamindus.22@cse.mrt.ac.lk>
2025-01-11 10:18:41 +0530

    Feat: Integrate email functionality with nodemailer, enhance image upload response, and update user model to include image URL and approval status

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,26 +1,28 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
-    index: { type: String, required: true },
-    numOfSeat: { type: Number, required: true },
-    seats: { type: [String], required: true },
-    users: [
-      {
-        username: { type: String, required: true },
-        email: { type: String, required: true },
-        whatsapp: { type: String, required: true },
-        department: { type: String, required: true },
-        foodList: { type: [String], required: true },
-        totalPrice: { type: Number, required: true },
-        batch: { type: String, required: true },
-        faculty: { type: String, required: true },
-        seatNumber: { type: String, default: undefined }, // Optional field
-      },
-    ],
-    createdAt: { type: Date, default: Date.now },
-  });
-  
-  // Check if the Order model already exists, otherwise create it
-  const User = mongoose.models.userModel || mongoose.model("userModel", orderSchema);
+  index: { type: String, required: true },
+  numOfSeat: { type: Number, required: true },
+  seats: { type: [String], required: true },
+  users: [
+    {
+      username: { type: String, required: true },
+      email: { type: String, required: true },
+      whatsapp: { type: String, required: true },
+      department: { type: String, required: true },
+      foodList: { type: [String], required: true },
+      totalPrice: { type: Number, required: true },
+      batch: { type: String, required: true },
+      faculty: { type: String, required: true },
+      seatNumber: { type: String, default: undefined }, // Optional field
+      imageURL: { type: String, required: true }, // New field
+      isApproved: { type: Boolean, required: [true, "Approval status is required"], default: false }, // New field
+    },
+  ],
+  createdAt: { type: Date, default: Date.now },
+});
+
+// Check if the Order model already exists, otherwise create it
+const User = mongoose.models.userModel || mongoose.model("userModel", orderSchema);
 
 export default User;
