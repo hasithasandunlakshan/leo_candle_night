c58e4e56ec767d39c118e22d011c6eab28d4c25e
hasithasandun <hasiofficial2002@gmail.com>
2025-02-09 03:40:31 +0530

    Enhance order form layout with improved styling and background elements

--- src/components/order/orderForm.tsx
+++ src/components/order/orderForm.tsx
@@ -19,9 +19,16 @@ export default function OrderForm() {
   };
 
   return (
-    <div className="min-h-screen bg-transparent flex items-center justify-center px-4 py-32">
-      <div className=" shadow-lg rounded-lg w-full max-w-lg p-8 border border-secondary">
-        <h1 className="text-3xl font-bold text-secondary text-center mb-6">
+    <div className=" min-h-screen bg-transparent flex items-center justify-center px-4 py-36 md:py-36  w-screen overflow-hidden">
+
+
+
+      <div className="relative z-10 md:w-[60%]  ">
+      <div className=" w-40 h-40 md:w-48 md:h-48 overflow-hidden -z-20 -top-20 opacity-45 absolute -left-20 bg-secondary rounded-full"></div>
+      <div className="w-40 h-40 md:w-48 md:h-48 -z-20 opacity-45 -bottom-20 overflow-hidden absolute -right-20 bg-secondary rounded-full"></div>
+<div className=" rounded-xl w-full   p-16   shadow-2xl shadow-slate-800 relative z-50  ">
+
+<h1 className="text-3xl font-bold text-secondary text-center mb-6">
         Retrieve Your Ticket Information
         </h1>
 
@@ -49,6 +56,8 @@ export default function OrderForm() {
 accurate for a smooth experience.
           </p>
         </div>
+</div>
+       
       </div>
     </div>
   );
